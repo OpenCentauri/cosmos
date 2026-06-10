295eda13d86fa3cef902f3a57a9180f40617183e
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-06-10 23:06:34 +0200

    Move some values to cosmos.conf

--- meta-opencentauri/recipes-data/config-manager/files/build-klipper-var-config.sh
+++ meta-opencentauri/recipes-data/config-manager/files/build-klipper-var-config.sh
@@ -12,6 +12,9 @@ get_config_value() {
 
 sync_camera_led_to_chamber_led=$(get_config_value sync_camera_led_to_chamber_led)
 camera_led_default_on=$(get_config_value camera_led_default_on)
+heatsoak=$(get_config_value heatsoak)
+adaptive_mesh=$(get_config_value adaptive_mesh)
+adaptive_purge=$(get_config_value adaptive_purge)
 full_calibrate_hotend_temperature=$(get_config_value full_calibrate_hotend_temperature)
 full_calibrate_bed_temperature=$(get_config_value full_calibrate_bed_temperature)
 bypass_calibration=$(get_config_value bypass_calibration)
@@ -24,6 +27,9 @@ cat > "${OUTPUT_FILE}" <<EOF
 [gcode_macro _COSMOS_SETTINGS]
 variable_sync_camera_led_to_chamber_led: ${sync_camera_led_to_chamber_led}
 variable_camera_led_default_on: ${camera_led_default_on}
+variable_heatsoak: ${heatsoak}
+variable_adaptive_mesh: ${adaptive_mesh}
+variable_adaptive_purge: ${adaptive_purge}
 variable_full_calibrate_hotend_temperature: ${full_calibrate_hotend_temperature}
 variable_full_calibrate_bed_temperature: ${full_calibrate_bed_temperature}
 variable_bypass_calibration: ${bypass_calibration}
